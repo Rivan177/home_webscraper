7e88ac39f02d9afcc7e457e428a8a2502045845b
Rivan177 <92820926+Rivan177@users.noreply.github.com>
2022-06-11 12:40:06 -0500

    fully working script

--- webdriver_setup.py
+++ webdriver_setup.py
@@ -1,14 +1,13 @@
 """
-This scripts sets up Selenium to access a website and brings up the listings of properties available for sale 
-in the state of Morelos, Mexico
+This scripts sets up Selenium to access a website and scrapes the listings of properties available for sale 
+in the state of Morelos, Mexico. It then writes a csv file with the obtained information. 
 """
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-#import pandas as pd
+from time import sleep
 import csv
 
 ## Functions 
@@ -23,10 +22,20 @@ def page_elements(xpath: str):
 ## To obtain a single of element
 def page_element(xpath: str):
     """
-    This funtion return an element with argument xpath as string
+    This funtion returns an element in the argument's xpath
     """
     elements = driver.find_element(By.XPATH, value=xpath)
     return elements
+
+## To go to the next page
+def click_next_page(xpath: str):
+    """
+    This funciton finds the next page button in xpath and click on it
+    """
+    next_page = page_element(xpath)
+    next_page.click()
+    sleep(10)
+    WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, xpath_characteristics)))
 
 
 ## Popular website in Mexico to find properties 
@@ -34,17 +43,6 @@ url = 'https://www.inmuebles24.com'
 ## Inputs for property type and location
 property_type = 'Casa'
 location_input = 'Morelos'
-
-## Specific path is not needed since chromedriver is in the same directory
-#path = 'D:\data_eng\home_webscraper'
-
-## Start webdirver with Chrome
-option = webdriver.ChromeOptions()
-## Removes navigation.webdriver flag 
-option.add_argument('--disable-blink-features=AutomationControlled')
-driver = webdriver.Chrome(options=option)
-driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-driver.get(url)
 
 ## XPaths for navigating landing page
 ## Button to click on "Comprar", On Sale properties
@@ -60,6 +58,26 @@ xpath_search_button = '//button[@data-qa="search-button"]'
 ## Next page button
 xpath_next_page = '//a[contains(@class, "gzANxl")]'
 
+## XPATHS for retreving information
+xpath_total_listings = '//h1[contains(@class, "postingsTitle")]'
+xpath_price = '//div[contains(@class, "components__Price-")]'
+xpath_characteristics = '//div[contains(@class, "PostingMain")]'
+xpath_location = '//div[contains(@class, "LocationLocation")]'
+
+## Specific path is not needed since chromedriver is in the same directory
+#path = 'D:\data_eng\home_webscraper'
+
+## Start webdirver with Chrome
+option = webdriver.ChromeOptions()
+## Removes navigation.webdriver flag 
+option.add_argument('--disable-blink-features=AutomationControlled')
+driver = webdriver.Chrome(options=option)
+driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+driver.get(url)
+
+## Wait for page to load, checking for property button present
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath_property_button)))
+
 ## Click on "Comprar"
 property_button = page_element(xpath_property_button)
 property_button.click()
@@ -72,10 +90,10 @@ select_property.select_by_visible_text(property_type)
 ## Input "Morelos" as location
 location_search_box = page_element(xpath_location_text_box)
 location_search_box.click()
-time.sleep(3)
+sleep(3)
 location_search_box.send_keys(location_input)
 ## Wait for autosuggestion list to appear
-wait = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, xpath_list_sugestions)))
+WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, xpath_list_sugestions)))
 
 ## Click on first item in location suggestions
 location_select = page_element(xpath_list_sugestions)
@@ -86,56 +104,51 @@ search_button = page_element(xpath_search_button)
 search_button.click()
 
 ## Allow time for resutls to load
-time.sleep(10)
+sleep(10)
+WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, xpath_characteristics)))
 
 ## Skip page 1 to avoid adds
-next_page = page_element(xpath_next_page)
-next_page.click()
-time.sleep(10)
+# click_next_page(xpath_next_page)
+# sleep(10)
+# WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, xpath_characteristics)))
+
 
 """
 Start scraping information about houses
 """
-## XPATHS for retreving information
-# xpath_containers = '//div[contains(@class, "postingCardstyles__PostingCardLayout")]'
-xpath_price = "//div[contains(@class, 'components__Price-')]"
-xpath_characteristics = '//div[contains(@class, "PostingMain")]'
-# xpath_area_total =
-# xpath_area_construction =
-# xpath_bedrooms =
-# xpath_restrooms = 
-# xpath_parking_spots =
-xpath_location = '//div[contains(@class, "LocationLocation")]'
+## Get total number of listings
+total_string = page_element(xpath_total_listings).text
+total_string = total_string[:6].replace(',', '')
+total_pages = round(int(total_string) / 20)
 
-## Create empty dataframe
-# houses = pd.DataFrame(columns=['Price', 'Characteristics', 'Location'])
-
+print(f'This script will scrape {total_pages} pages, expecting {int(total_string) - 5} reults')
 
 ## Testing code to get a single element   
-# price1 = page_results[0].find_element(By.CLASS_NAME, "price").text
-# print(f'Second home element{page_results[0]}')
+# price1 = page_results[1].find_element(By.CLASS_NAME, "price").text
+# print(f'Second home element{page_results[1]}')
 # print(f'Second home price {price1}')
 
-price_values = page_elements(xpath_price)
-characteristics_values = page_elements(xpath_characteristics)
-location_values = page_elements(xpath_location)
 
-homes_list = list()
+with open('ind_home.csv', 'w', newline='', encoding='utf-8') as csv_file:
+    writer = csv.writer(csv_file)
 
-with open('ind_home.csv', 'w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
-    for price, characteritics, location in zip(price_values, characteristics_values, location_values):
-        print(characteritics.text, "\n", price.text, "\n", location.text)
-        temp_list = [price.text, characteritics.text, location.text]
-        writer.writerow(temp_list)
-        homes_list.append(temp_list)
-        time.sleep(1)
-    # item = result.find_element(By.CLASS_NAME, "price").text
-    # price_list.append(item)
+    page = 1
 
-#homes_list = '|'.join(homes_list)
-print(homes_list)
+    while page < total_pages+1:
+        print(f'Scrapping page {page}...')
+        print(f'{total_pages - page} pages pending to scrape')
 
+        price_values = page_elements(xpath_price)
+        characteristics_values = page_elements(xpath_characteristics)
+        location_values = page_elements(xpath_location)
+
+        for price, characteritics, location in zip(price_values, characteristics_values, location_values):
+            temp_list = [price.text, characteritics.text, location.text]
+            writer.writerow(temp_list)
+            sleep(0.1)
+
+        click_next_page(xpath_next_page)
+        page += 1
 
 # def get_item_by_class(page_results, value):
 #     '''
@@ -152,3 +165,7 @@ print(homes_list)
 #     return info_list
 
 # get_item_by_class(page_results, "price")
+
+### Work in progress, add checks for elements, add function to handle nulls. add function to page through based on the total results. 
+
+driver.quit()
